8b6419beb4b568d5f18e8551b882b00f09852cd9
Harry Zhang <harryzyz@gmail.com>
2022-07-29 11:18:42 -0500

    update

--- Module 1 - Introduction to Java/2. Variables/Challenge 2/Chatbot.java
+++ Module 1 - Introduction to Java/2. Variables/Challenge 2/Chatbot.java
@@ -28,13 +28,15 @@ public class Chatbot {
 
         //add new a line before asking next question.
         System.out.println("\n So you're " + age + ", cool! I'm 400 years old.");
-        System.out.println("This means I'm <divide 400 by age> times older than you.");
+        System.out.println("This means I'm " + (400/age) + " times older than you.");
         System.out.println("Enough about me. What's your favourite language? (just don't say Python)");
         //Pick up language and store it. 
+        scan.nextLine();
+        String language = scan.nextLine();
 
 
         //add new a line here.
-        System.out.println("<language>, that's great! Nice chatting with you <name>. I have to log off now. See ya!");
+        System.out.println("\n" + language + ", that's great! Nice chatting with you " + (name) + ". I have to log off now. See ya!");
         
         //close scanner. 
         scan.close();
