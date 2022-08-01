1ae6d84e855e7c4a5bea0e88c9a8a7367a397d59
Harry Zhang <harryzyz@gmail.com>
2022-08-01 10:42:15 -0500

    save

--- section seven/Car.java
+++ section seven/Car.java
@@ -1,10 +1,32 @@
 public class Car {
     
-    String make;
-    double price;
-    int year;
-    String color;
+    private String make;
+    private double price;
+    private int year;
+    private String color;
 
+    public Car(String make, double price, int year, String color) {
+        this.make = make;
+        this.price = price;
+        this.year = year;
+        this.color = color;
+    }
+
+    public String getMake() {
+        return this.make;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
 
 
 
--- section seven/Main.java
+++ section seven/Main.java
@@ -1,21 +1,14 @@
 public class Main{
     public static void main(String[] args) {
-        Car nissan = new Car();
+        Car nissan = new Car("Nissan", 5000, 2020, "red");
 
-        nissan.make = "Nissan";
-        nissan.price = 5000;
-        nissan.year = 2020;
-        nissan.color = "red";
+        
+        Car dodge = new Car("Dodge", 11000, 2019, "blue");
+        
 
-        Car dodge = new Car();
-        dodge.make = "Dodge";
-        dodge.price = 11000;
-        dodge.year = 2019;
-        dodge.color = "blue";
+        System.out.println("This " + nissan.getMake() + " is worth $" + nissan.getPrice() + ". It was built in " + nissan.getYear() + ". It is " + nissan.getColor() + ".\n");
 
-        System.out.println("This " + nissan.make + " is worth $" + nissan.price + ". It was built in " + nissan.year + ". It is " + nissan.color + ".\n");
-
-        System.out.println("This " + dodge.make + " is worth $" + dodge.price + ". It was built in " + dodge.year + ". It is " + dodge.color + ".\n");
+        System.out.println("This " + dodge.getMake() + " is worth $" + dodge.getPrice() + ". It was built in " + dodge.getYear() + ". It is " + dodge.getColor() + ".\n");
 
     }
 }
