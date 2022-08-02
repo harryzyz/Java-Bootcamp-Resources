7a5f8ae44f1edf04b0bb5838f75906f4b35427ec
Harry Zhang <harryzyz@gmail.com>
2022-08-01 23:42:13 -0500

    cars class

---
+++ section seven/Dealership.java
@@ -0,0 +1,13 @@
+// import java.util.Arrays;
+
+public class Dealership {
+    
+    private Car[] cars;
+
+    public Dealership() {
+        this.cars = new Car[3];
+        
+    }
+
+
+}
--- section seven/Main.java
+++ section seven/Main.java
@@ -1,14 +1,43 @@
+import java.util.Arrays;
+
 public class Main{
     public static void main(String[] args) {
-        Car nissan = new Car("Nissan", 5000, 2020, "red");
 
+        String[] parts = {"tires", "keys"};
+
+        Car nissan = new Car("Nissan", 5000, 2020, "red", parts);
         
-        Car dodge = new Car("Dodge", 11000, 2019, "blue");
+        Car dodge = new Car("Dodge", 11000, 2019, "blue", parts);
+
+        Car nissan2 = new Car(nissan);
         
+        nissan2.setColor("yellow");
+        nissan2.setParts(new String[] {"tires", "filter"});
 
-        System.out.println("This " + nissan.getMake() + " is worth $" + nissan.getPrice() + ". It was built in " + nissan.getYear() + ". It is " + nissan.getColor() + ".\n");
+        Dealership dealership = new Dealership();
 
-        System.out.println("This " + dodge.getMake() + " is worth $" + dodge.getPrice() + ". It was built in " + dodge.getYear() + ". It is " + dodge.getColor() + ".\n");
+        // System.out.println(nissan);
+
+
+
+        // System.out.println(Arrays.toString(nissan2.getParts()));
+
+
+        // nissan.drive();
+        // dodge.drive();
+        // nissan2.drive();
+        // dodge.setColor("jet black");
+
+
+        // nissan.setPrice(nissan.getPrice() / 2);
+
+        // dodge.setPrice(dodge.getPrice() / 2);
+
+        // System.out.println("This " + nissan.getMake() + " is worth $" + nissan.getPrice() + ". It was built in " + nissan.getYear() + ". It is " + nissan.getColor() + ".\n");
+
+        // System.out.println("This " + dodge.getMake() + " is worth $" + dodge.getPrice() + ". It was built in " + dodge.getYear() + ". It is " + dodge.getColor() + ".\n");
+
+        // System.out.println("This " + nissan2.getMake() + " is worth $" + nissan2.getPrice() + ". It was built in " + nissan2.getYear() + ". It is " + nissan2.getColor() + ".\n");
 
     }
 }
