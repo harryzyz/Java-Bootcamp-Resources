de1b0495ec7b11cdf7b2d283487ebbafaffcd004
Harry Zhang <harryzyz@gmail.com>
2022-08-03 10:57:38 -0500

    update

--- Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Lessons/intro-to-debugging/UpperTriangular.java
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Debugging Lessons/intro-to-debugging/UpperTriangular.java
@@ -9,7 +9,7 @@ public class UpperTriangular {
         };
 
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; i++) {
+            for (int j = 0; j < i; j++) {
                 matrix[i][j] = 0;
             }
         }
