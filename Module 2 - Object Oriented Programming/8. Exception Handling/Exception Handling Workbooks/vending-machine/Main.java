1392d0a3230ec474f8fb0d3e96f88fb925daed0b
Harry Zhang <harryzyz@gmail.com>
2022-08-05 21:43:05 -0500

    vending machine

--- Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/vending-machine/Main.java
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/vending-machine/Main.java
@@ -13,10 +13,10 @@ public class Main {
         System.out.println("\t************************************************");
 
         Item[][] items = new Item[][] {
-            { new Item("Pepsi", 1.99, -3) , new Item("Fresca", 1.49, 3), new Item("     ", 2.49, -2) },
-            { new Item("Fanta", 1.99, 2) , new Item("      ", 1.49, 2), new Item("A & W", 2.49, 3) },
-            { new Item("     ", 1.99, 2) , new Item("C-Cola", 1.49, 2), new Item("Berry", -2.49, 1) }
-        };   
+                { new Item("Pepsi", 1.99, 3), new Item("Fresca", 1.49, 3), new Item("Brisk", 2.49, 2) },
+                { new Item("Fanta", 1.99, 2), new Item("Barq's", 1.49, 2), new Item("A & W", 2.49, 3) },
+                { new Item("Crush", 1.99, 2), new Item("C-Cola", 1.49, 2), new Item("Berry", 2.49, 1) }
+        };
 
         Machine machine = new Machine(items);
 
@@ -24,12 +24,26 @@ public class Main {
 
         while (true) {
             System.out.print("Pick a row: ");
-            int row = scan.nextInt();
-            System.out.print("Pick a spot in the row: ");
-            int spot = scan.nextInt();
+            int row = scan.hasNextInt() ? scan.nextInt() : 404;
+            scan.nextLine();
 
-            machine.dispense(row, spot);
+            System.out.print("Pick a spot in the row: ");
+            int spot = scan.hasNextInt() ? scan.nextInt() : 404;
+            scan.nextLine();
+
+            if (row == 404 || spot == 404) {
+                System.out.println("INVALID INPUT");
+                continue;
+            } else if (row < 0 || row > machine.getLength()[0] - 1 || spot < 0 || spot > machine.getLength()[1] - 1) {
+                System.out.println("INVALID RANGE");
+                continue;
+            } else if (machine.getItem(row, spot).getQuantity() == 0 ) {
+                System.out.println("Empty Slot");
+                continue;
+            }
+
             System.out.println("\n" + machine);
+            machine.dispense(row, spot);
             System.out.print("\nEnjoy your drink! Press 1 to purchase another: ");
             if (!scan.next().equalsIgnoreCase("1")) {
                 break;
--- Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/vending-machine/models/Machine.java
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/vending-machine/models/Machine.java
@@ -1,4 +1,5 @@
 package models;
+
 public class Machine {
     private Item[][] items;
 
@@ -10,17 +11,24 @@ public class Machine {
             }
         }
     }
-    
+
     public Item getItem(int row, int spot) {
         return new Item(this.items[row][spot]);
     }
-    
+
     public void setItem(Item item, int row, int spot) {
         this.items[row][spot] = new Item(item);
     }
 
     public void dispense(int row, int spot) {
+        if (this.items[row][spot].getQuantity() == 0) {
+            throw new IllegalArgumentException("cannot dispense an item with quantity zero.");
+        }
         items[row][spot].setQuantity(items[row][spot].getQuantity() - 1);
+    }
+
+    public int[] getLength() {
+        return new int[] { this.items.length, this.items[0].length };
     }
 
     public String toString() {
