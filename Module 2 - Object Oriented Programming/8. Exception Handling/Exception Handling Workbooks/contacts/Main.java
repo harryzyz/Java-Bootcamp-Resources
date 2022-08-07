c437858c76fd059a2dc72cd8b22d0e8a0cef4d8e
Harry Zhang <harryzyz@gmail.com>
2022-08-06 22:45:02 -0500

    contactsmanager

--- Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/contacts/Main.java
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/contacts/Main.java
@@ -1,32 +1,78 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.text.ParseException;
+import java.util.Scanner;
 
 import models.Contact;
 import models.ContactManager;
 
 public class Main {
+    static ContactManager manager = new ContactManager();
+
     public static void main(String[] args) {
         try {
-            Contact contact = new Contact("Alan Turing", "7205312345", "07/23/1912");
-            System.out.println(contact);
-        } catch (ParseException e) {
+            loadContacts("contacts.txt");
+        } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         } finally {
-            System.out.println("Process Complete.");
+            System.out.println("CONTACTS LOADED\n\n");
+            System.out.println(manager);
+            manageContacts();
         }
+
     }
 
     /**
      * Name: manageContacts
      *
      * Inside the function:
-     * • 1. Starts a new instance of Scanner;
-     * • 2. In an infinite loop, the user can choose to a) add b) remove a contact
+     * - 1. Starts a new instance of Scanner;
+     * - 2. In an infinite loop, the user can choose to a) add b) remove a contact
      * c) exit.
-     * • case a: ask for the name, phone number and birthDate.
-     * • case b: ask who they'd like to remove.
-     * • case c: break the loop.
-     * • 3. close Scanner.
+     * - case a: ask for the name, phone number and birthDate.
+     * - case b: ask who they'd like to remove.
+     * - case c: break the loop.
+     * - 3. close Scanner.
      */
+    public static void manageContacts() {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.println("Please choose: \na) - add a contact \nb) - remove a contact \nc) - exit ");
+            String choice = scan.nextLine();
+            if (choice.equals("a")) {
+
+                System.out.print("please add name:");
+                String name = scan.nextLine();
+                System.out.print("please add phone number:");
+                String phoneNumber = scan.nextLine();
+                System.out.print("please add birth date:");
+                String birthDate = scan.nextLine();
+                if (name.isBlank() || phoneNumber.isBlank() || phoneNumber.length() < 5) {
+                    System.out.println("\nThe input your provided is not valide. Registration failed.");
+                } else {
+                    try {
+                        manager.addContact(new Contact(name, phoneNumber, birthDate));
+                    } catch (ParseException e) {
+                        System.out.println(e.getMessage());
+                    } finally {
+                        System.out.println("\n\nUPDATED CONTACTS\n\n" + manager);
+                    }
+                }
+
+            } else if (choice.equals("b")) {
+
+                System.out.println("please provide the contact name to remove");
+                String name = scan.nextLine();
+                manager.removeContact(name);
+                System.out.println("\n\nUPDATED CONTACTS\n\n" + manager);
+            } else {
+                break;
+            }
+        }
+
+        scan.close();
+
+    }
 
     /**
      * Name: loadContacts
@@ -35,11 +81,37 @@ public class Main {
      * @throws FileNotFoundException
      *
      *                               Inside the function:
-     *                               • 1. loads contacts from <fileName>;
-     *                               • 2. From the manager object, it adds all
+     *                               - 1. loads contacts from <fileName>;
+     *                               - 2. From the manager object, it adds all
      *                               contacts to the contacts list.
      *                               Hint: use scan.next to grab the next String
      *                               separated by white space.
      */
-
+    public static void loadContacts(String fileName) throws FileNotFoundException {
+        FileInputStream fis = new FileInputStream(fileName);
+        Scanner scanfile = new Scanner(fis);
+        while (scanfile.hasNextLine()) {
+            try {
+                Contact contact = new Contact(scanfile.next(), scanfile.next(), scanfile.next());
+                manager.addContact(contact);
+            } catch (ParseException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        scanfile.close();
+    }
 }
+
+// try {
+// ContactManager manager = new ContactManager();
+// manager.addContact(new Contact("Ryan", "6135012424", "11/11/1992"));
+// manager.addContact(new Contact("Gio", "6477092344 ", "11/11/1993"));
+// manager.addContact(new Contact("Thomas", "8192256979 ", "11/11/1994"));
+// manager.removeContact("Gio");
+// System.out.println(manager);
+// } catch (ParseException e) {
+// System.out.println(e.getMessage());
+// } finally {
+// System.out.println("Process Complete.");
+// }
+// }
--- Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/contacts/models/Contact.java
+++ Module 2 - Object Oriented Programming/8. Exception Handling/Exception Handling Workbooks/contacts/models/Contact.java
@@ -10,69 +10,105 @@ public class Contact {
     private int age;
     private String birthDate;
     private String phoneNumber;
-    
-    public Contact(String name, String birthDate, String phoneNumber) throws ParseException {
+
+    public Contact(String name, String phoneNumber, String birthDate) throws ParseException {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be null/blank.");
+        }
+
+        if (phoneNumber == null || phoneNumber.isBlank()) {
+            throw new IllegalArgumentException("Phone Number cannot be null/blank.");
+        }
+
+        if (phoneNumber.length() < 5) {
+            throw new IllegalArgumentException("Phone Number must have at least 5 digits.");
+        }
+
         this.name = name;
         this.birthDate = birthDate;
         this.phoneNumber = phoneNumber;
-        String pattern = "MM/dd/yyyy";
-        SimpleDateFormat formatter = new SimpleDateFormat(pattern);
-        formatter.setLenient(false);
-        long diff = new Date().getTime() - formatter.parse(birthDate).getTime();
-        this.age = (int) (TimeUnit.MILLISECONDS.toDays(diff) / 365) ;
+        this.age = toAge(birthDate);
+
     }
-    
+
     public Contact(Contact source) {
         this.name = source.name;
         this.age = source.age;
         this.birthDate = source.birthDate;
         this.phoneNumber = source.phoneNumber;
     }
-    
+
     public int getAge() {
         return age;
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
     public String getBirthDate() {
         return birthDate;
     }
-    
+
     public String getPhoneNumber() {
         return phoneNumber;
     }
-    
-    public void setAge(int age) {
-        // String pattern = "MM/dd/yyyy";
-        // SimpleDateFormat formatter = new SimpleDateFormat(pattern);
-        // formatter.setLenient(false);
-        // long diff = new Date().getTime() - formatter.parse(birthDate).getTime();
-        // this.age = (int) (TimeUnit.MILLISECONDS.toDays(diff) / 365);
-        this.age = age;
-    }
 
     public void setName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be null/blank.");
+        }
         this.name = name;
     }
 
-    public void setBirthDate(String birthDate) {
+    public void setBirthDate(String birthDate) throws ParseException {
         this.birthDate = birthDate;
+        setAge(toAge(birthDate));
     }
 
     public void setPhoneNumber(String phoneNumber) {
+        if (phoneNumber == null || phoneNumber.isBlank()) {
+            throw new IllegalArgumentException("Phone Number cannot be null/blank.");
+        }
+
+        if (phoneNumber.length() < 5) {
+            throw new IllegalArgumentException("Phone Number must have at least 5 digits.");
+        }
+
         this.phoneNumber = phoneNumber;
     }
 
-    public String toString() {
-        return "Name: " + this.name + " Age: " + this.age + " Birth Date: " + this.birthDate + " Phone Number: " + this.phoneNumber;
+    private void setAge(int age) {
+        this.age = age;
     }
 
-    
-  
-  
-  
+    /**
+     * Name: toAge
+     * 
+     * @param birthDate
+     * @return age (int)
+     * @throws ParseException
+     *
+     *                        Inside the function:
+     *                        1. Parses a date from the birthDate String
+     *                        2. Gets the current date
+     *                        3. Subtracts the difference and returns the age.
+     *
+     */
+    public int toAge(String birthDate) throws ParseException {
+        String pattern = "MM/dd/yyyy";
+        SimpleDateFormat formatter = new SimpleDateFormat(pattern);
+        formatter.setLenient(false);
+        long diff = new Date().getTime() - formatter.parse(birthDate).getTime();
+        int age = (int) (TimeUnit.MILLISECONDS.toDays(diff) / 365);
+        return age;
+    }
+
+    public String toString() {
+        return "Name: " + this.name +
+                "\nPhone Number: " + this.phoneNumber +
+                "\nBirth Date: " + this.birthDate + "\nAge: " +
+                this.age + " year old\n";
+    }
 
 }
