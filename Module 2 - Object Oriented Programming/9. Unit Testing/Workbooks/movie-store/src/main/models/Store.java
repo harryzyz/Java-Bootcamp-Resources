636c180f6519511179c2adfb43c4b8d644b7b878
Harry Zhang <harryzyz@gmail.com>
2022-08-08 00:14:24 -0500

    movie store unit test

--- Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/movie-store/src/main/models/Store.java
+++ Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/movie-store/src/main/models/Store.java
@@ -1,6 +1,7 @@
 package src.main.models;
 
 import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 public class Store {
     ArrayList<Movie> movies;
@@ -15,6 +16,43 @@ public class Store {
 
     public void setMovie(int index, Movie movie) {
         this.movies.set(index, new Movie(movie));
+    }
+
+    public void addMovie(Movie movie) {
+        this.movies.add(new Movie(movie));
+    }
+
+    public void sellMovie(String name) {
+        if (!this.movies.get(getMovieIndex(name)).isAvailable()) {
+            throw new IllegalStateException("cannot sell rented movie");
+        }
+        this.movies.removeIf((movie) -> movie.getName().equals(name));
+    }
+
+    public void rentMovie(String name) {
+        if (!this.movies.get(getMovieIndex(name)).isAvailable()) {
+            throw new IllegalStateException("cannot rent rented movie");
+        }
+        this.movies.get(getMovieIndex(name)).setAvailable(false);
+    }
+
+    public void returnMovie(String name) {
+        if (this.movies.get(getMovieIndex(name)).isAvailable()) {
+            throw new IllegalStateException("cannot return available movie");
+        }
+        this.movies.get(getMovieIndex(name)).setAvailable(true);
+    }
+
+    public int getMovieIndex(String name) {
+        return IntStream.range(0, this.movies.size())
+            .filter((i) -> this.movies.get(i).getName().equals(name))
+            .findFirst()
+            .orElse(-1);
+    }
+
+    public boolean contains(Movie movie) {
+        // return false;
+        return this.movies.contains(movie);
     }
 
     public String toString() {
--- Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/movie-store/src/test/StoreTest.java
+++ Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/movie-store/src/test/StoreTest.java
@@ -1,9 +1,66 @@
 package src.test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import src.main.models.Movie;
 import src.main.models.Store;
 
 public class StoreTest {
 
     Store store;
+
+    @Before
+    public void setup() {
+        store = new Store();
+        store.addMovie(new Movie("The Shawshank Redemption", "Blue-Ray", 9.2));
+        store.addMovie(new Movie("The Godfather", "Blue-Ray", 9.1));
+    }
+
+    @Test
+    public void movieAdded() {
+        assertTrue(store.contains(new Movie("The Godfather", "Blue-Ray", 9.1)));
+    }
+
+    @Test
+    public void sellMovieTest() {
+        store.sellMovie("The Shawshank Redemption");
+        assertFalse(store.contains(new Movie("The Shawshank Redemption", "Blue-Ray", 9.2)));
+    }
+
+    @Test
+    public void rentMovieTest() {
+        store.rentMovie("The Godfather");
+        assertFalse(store.getMovie(1).isAvailable());
+    }
+
+    @Test
+    public void returnMovieTest() {
+        store.rentMovie("The Godfather");
+        store.returnMovie("The Godfather");
+        assertTrue(store.getMovie(1).isAvailable());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void movieNotInStock() {
+        store.rentMovie("The Godfather");
+        store.sellMovie("The Godfather");
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void returnAvailableMovie() {
+        store.returnMovie("The Godfather");
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void rentRentedMovie() {
+        store.rentMovie("The Godfather");
+        store.rentMovie("The Godfather");
+    }
+
+
 
 }
