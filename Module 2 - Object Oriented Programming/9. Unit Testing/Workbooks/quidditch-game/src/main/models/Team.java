c53c55b4ec42c7b608279541e3ac43b1cd37762a
Harry Zhang <harryzyz@gmail.com>
2022-08-09 00:23:48 -0500

    update

--- Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/quidditch-game/src/main/models/Team.java
+++ Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/quidditch-game/src/main/models/Team.java
@@ -1,29 +1,141 @@
 package src.main.models;
 
+import java.util.Arrays;
+
 public class Team {
 
     private static final String POSITION_CHASER = "chaser";
     private static final String POSITION_SEEKER = "seeker";
     private static final String POSITION_KEEPER = "keeper";
+    private String house;
+    private String keeper;
+    private String seeker;
+    private String[] chasers;
 
-/* FREQUENTLY ASKED QUESTIONS:
+    /*
+     * FREQUENTLY ASKED QUESTIONS:
+     * 
+     * Question: the constants are final, so why can't we make them public? It's not
+     * possible for the caller to update them.
+     * Answer: Even if the constant is final, I prefer to expose a method instead of
+     * the variable. But if you want to expose the variable, that's also correct.
+     * 
+     */
     
-Question: the constants are final, so why can't we make them public? It's not possible for the caller to update them.
-  Answer: Even if the constant is final, I prefer to expose a method instead of the variable. But if you want to expose the variable, that's also correct.
 
-*/
+    public Team(String house, String keeper, String seeker, String[] chasers) {
+        checkParam(house);
+        checkParam(keeper);
+        checkParam(seeker);
+        if (hasBlank(chasers) || hasNull(chasers)) {
+            throw new IllegalArgumentException("chasers cannot be null/blank");
+        }
+        if (chasers.length != 3) {
+            throw new IllegalArgumentException("there mush be 3 chasers");
+        }
+        this.house = house;
+        this.keeper = keeper;
+        this.seeker = seeker;
+        this.chasers = Arrays.copyOf(chasers, chasers.length);
+    }
 
-    
-     public static String getPositionChaser() {
-         return POSITION_CHASER;
-     }
+    public Team(Team source) {
+        this.house = source.house;
+        this.keeper = source.keeper;
+        this.seeker = source.seeker;
+        this.chasers = Arrays.copyOf(source.chasers, source.chasers.length);
+    }
 
-     public static String getPositionSeeker() {
-         return POSITION_SEEKER;
-     }
+    public static String getPositionChaser() {
+        return POSITION_CHASER;
+    }
 
-     public static String getPositionKeeper() {
-         return POSITION_KEEPER;
-     }
+    public static String getPositionSeeker() {
+        return POSITION_SEEKER;
+    }
+
+    public static String getPositionKeeper() {
+        return POSITION_KEEPER;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public String getKeeper() {
+        return keeper;
+    }
+
+    public String getSeeker() {
+        return seeker;
+    }
+
+    public String[] getChasers() {
+        return Arrays.copyOf(chasers, chasers.length);
+    }
+
+    public void setHouse(String house) {
+        checkParam(house);
+        this.house = house;
+    }
+
+    public void setKeeper(String keeper) {
+        checkParam(keeper);
+        this.keeper = keeper;
+    }
+
+    public void setSeeker(String seeker) {
+        checkParam(seeker);
+        this.seeker = seeker;
+    }
+
+    public void setChasers(String[] chasers) {
+        if (hasBlank(chasers) || hasNull(chasers)) {
+            throw new IllegalArgumentException("chasers cannot be null/blank");
+        }
+        if (chasers.length != 3) {
+            throw new IllegalArgumentException("there mush be 3 chasers");
+        }
+        this.chasers = Arrays.copyOf(chasers, chasers.length);
+    }
+
+    public void checkParam(String param) {
+        if (param == null || param.isBlank()) {
+            throw new IllegalArgumentException(param + " cannot be null or blank");
+        }
+    }
+
+    /**
+     * Function name: hasNull
+     * @param array
+     * @return (boolean)
+     * 
+     * Inside the function:
+     *    1. return true if the array contain null element.
+     */
+    public static boolean hasNull(String[] array) {
+        return Arrays.stream(array)
+        .anyMatch((item) -> item == null);
+    }
+
+    /**
+     * Function name: hasBlank
+     * @param array
+     * @return
+     * 
+     * Inside this function:
+     *    1. return true if the array contains blank element.
+     */
+    public static boolean hasBlank(String[] array) {
+        return Arrays.stream(array)
+        .anyMatch((item) -> item.isBlank());
+    }
+
+    public String toString() {
+        return "House: " + this.house + "\n" +
+          "Keeper: " + this.keeper + "\n" +         
+          "Seeker: "  + this.seeker + "\n" +         
+          "Chasers: " + Arrays.toString(this.chasers) + "\n"; 
+    }
 
 }
--- Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/quidditch-game/src/test/TeamTest.java
+++ Module 2 - Object Oriented Programming/9. Unit Testing/Workbooks/quidditch-game/src/test/TeamTest.java
@@ -1,6 +1,28 @@
 package src.test;
 
-public class TeamTest {
-    
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import src.main.models.Team;
+
+public class TeamTest {
+
+    @Before
+    public void setup() {
+        // Team team = new Team();
+    }
+    
+    @Test
+    public void hasNullTest() {
+        String[] chasers = new String[] {null, "Ginny", "Katie"};
+        assertTrue(Team.hasNull(chasers));
+    }
+
+    @Test
+    public void hasBlankTest() {
+        String[] chasers = new String[] {"    ", "Ginny", "Katie"}; 
+        assertTrue(Team.hasBlank(chasers));
+    }
 }
