b201d58ff1d2c84032433e484fdfb2f80b439f1d
Harry Zhang <harryzyz@gmail.com>
2022-08-13 00:38:27 -0500

    update

--- Module 2 - Object Oriented Programming/11. Concurrency/intro-to-concurrency/MultiThread.java
+++ Module 2 - Object Oriented Programming/11. Concurrency/intro-to-concurrency/MultiThread.java
@@ -1,16 +1,21 @@
 public class MultiThread {
 
-    public static void main(String[] args) {
+    
 
+    public static void main(String[] args) {
+        
+        
+        Thread thread2 = new Thread(() -> longTask());
+        thread2.start();
         System.out.println("Doing other work on main thread");
         
     }
 
     public static void longTask() {
-        long t= System.currentTimeMillis();
-        long end = t+3000;
+        long t = System.currentTimeMillis();
+        long end = t + 3000;
 
-        while(true) {
+        while (true) {
             if (System.currentTimeMillis() == end) {
                 System.out.println("Finished long task");
                 break;
